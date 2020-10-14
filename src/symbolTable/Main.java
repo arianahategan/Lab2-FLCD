edfe2ce0f60d1540882375c5e9bd3de1e3cd1f47
Ariana Hategan <arianahategan@arianas-mbp.home>
2020-10-14 08:16:02 +0300

    some changes

--- src/symbolTable/Main.java
+++ src/symbolTable/Main.java
@@ -9,12 +9,13 @@ public class Main {
 		symbolTable.insert(15);
 	    symbolTable.insert(1);
 		symbolTable.insert(3);
+		symbolTable.insert(2);
 		symbolTable.insert(4);
 		symbolTable.insert(6);
-		symbolTable.insert(2);
+		symbolTable.insert(22);
 
 		if(symbolTable.search(22))
-	        System.out.println("ok");
+	        System.out.println("found");
 
 		new BinaryTreePrinter(binarySearchTree.getRoot()).print(System.out);
     }
